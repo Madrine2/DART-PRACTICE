58f36cd2787ac851279c95dfa838914591b16258
Namlindwa Madrine <namulindwamadrine2@gmail.com>
2022-11-21 07:59:39 -0800

    my firt commit for review

---
+++ 1_intro.dart
@@ -0,0 +1,7 @@
+void main(){
+    //print("Hello World");
+    var name = "";
+    print("What is your name?");
+    name = srdin.readLineSync().toString();
+    print("Your name is $name");
+}
---
+++ 4_for_loop.dart
@@ -0,0 +1,11 @@
+// A loop is an operation in a programming language that will help us to to execute a ertain piece f code repeatedly until a certain condition is reached.
+int main(){
+for int(int i = 0; i <= 100; i++) {
+    print("$i. Madrine");
+     }
+    return 0;
+} 
+
+for(int i = 1000; i > 0; i--) {
+    print("this is $i time")
+}
